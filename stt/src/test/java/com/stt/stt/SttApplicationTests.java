355e220e144db09d3fd32bf5fdaa2de5b3583aff
root <root>
2018-12-17 18:44:04 +0800

    上传springboottest

---
+++ stt/src/test/java/com/stt/stt/SttApplicationTests.java
@@ -0,0 +1,17 @@
+package com.stt.stt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SttApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
